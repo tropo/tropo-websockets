e508d015dc15fe19da1a4eb772cc984ea1cb830f
Jason Goecke <jason@goecke.net>
2010-12-17 15:19:29 -0800

    Added the HTML page.

--- examples/subscriber.rb
+++ examples/subscriber.rb
@@ -4,7 +4,7 @@ require 'awesome_print'
 require 'json'
 
 # Connects to Web Socket server at host example.com port 10081.
-client = WebSocket.new("ws://0.0.0.0:8082")
+client = WebSocket.new("ws://sandite.orl.voxeo.net:8082")
 # Sends a message to the server.
 client.send({ :type => 'subscriber', :id => 'tropo-app1' }.to_json)
 while data = client.receive()
